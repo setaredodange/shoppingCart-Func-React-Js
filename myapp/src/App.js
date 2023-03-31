66d518c4776188da3928677bb4f215b4af6aba30
somayehdodangeh <dodangeh7019@gmail.com>
2023-03-31 03:20:20 -0700

    1th

---
+++ myapp/src/App.js
@@ -0,0 +1,22 @@
+import React  from 'react'
+import Header from "./components/Header/Header"
+import Footer  from './components/Footer/Footer'
+import Shop from './components/Shop/Shop'
+
+ function App  () {
+
+    return (
+      <div>
+
+        <Header/>
+
+        <Shop/>
+
+
+       <Footer/>
+        
+      </div>
+    )
+
+}
+export default App
